7877ade26e95f52323982ac1bc4b20295a92d88d
Mulraj Gupta <mulrajgupta208@gmail.com>
2025-07-07 10:59:24 +0530

    chore: convert code to es5

--- calver-plugin.js
+++ calver-plugin.js
@@ -1,45 +1,45 @@
-'use strict';
+'use strict'
 
-import { Plugin } from 'release-it';
-import calver from 'calver';
-const DEFAULT_FORMAT = 'yy.mm.minor',
-    DEFAULT_INCREMENT = 'calendar',
-    FALLBACK_INCREMENT = 'minor';
+const calver = require('calver')
+const { Plugin } = require('release-it')
+
+const DEFAULT_FORMAT = 'yy.mm.minor'
+const DEFAULT_INCREMENT = 'calendar'
+const FALLBACK_INCREMENT = 'minor'
 
 class CalverPlugin extends Plugin {
-
     getFormat() {
-        return this.getContext().format || DEFAULT_FORMAT;
+        return this.getContext().format || DEFAULT_FORMAT
     }
 
     getInc() {
-        return this.getContext().increment || DEFAULT_INCREMENT;
+        return this.getContext().increment || DEFAULT_INCREMENT
     }
 
     getFallbackInc() {
-        return this.getContext().fallbackIncrement || FALLBACK_INCREMENT;
+        return this.getContext().fallbackIncrement || FALLBACK_INCREMENT
     }
 
     getIncrementedVersion(args) {
-        const {latestVersion} = args || {};
+        const { latestVersion } = args || {}
         try {
-            return calver.inc(this.getFormat(), latestVersion, this.getInc());
+            return calver.inc(this.getFormat(), latestVersion, this.getInc())
         } catch {
             try {
-                return calver.inc(this.getFormat(), latestVersion, this.getFallbackInc());
+                return calver.inc(this.getFormat(), latestVersion, this.getFallbackInc())
             } catch {
-                return latestVersion;
+                return latestVersion
             }
         }
     }
 
     getIncrementedVersionCI() {
-      return this.getIncrementedVersion(...arguments);
+        return this.getIncrementedVersion(...arguments)
     }
 
     getIncrement() {
-      return this.getIncrementedVersion(...arguments);
+        return this.getIncrementedVersion(...arguments)
     }
 }
 
-export default CalverPlugin;
+module.exports = CalverPlugin
